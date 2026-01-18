6c2c2d26da49f8ec1eadd26000cd0a3195f57118
pramebt <bdforwk@gmail.com>
2026-01-18 21:57:43 +0700

    Update API_BASE_URL in constants.js: Changed the API base URL to use VITE_API_URL for improved configuration management.

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,5 +1,5 @@
 // API Base URL
-export const API_BASE_URL = import.meta.env.VITE_API_BASE_URL || 'http://localhost:5000';
+export const API_BASE_URL = import.meta.env.VITE_API_URL ;
 
 // Order Status
 export const ORDER_STATUS = {
